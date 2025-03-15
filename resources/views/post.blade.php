4ee45f4571bf756056a7a6c42158702a5b23a0cd
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2025-03-15 17:05:40 +0600

    layout added

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,21 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="/css/app.css">
-    <script src="/js/app.js"></script>
-</head>
-<body>
-    <article>
-        <h1><?= $post->title ?></h1>
-        <div>
-            <?= $post->body ?>
-        </div>
-        <a href="/">Go Back</a>
-    </article>
-  
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+<article>
+    <h1>{{ $post->title }}</h1>
+    <div>
+        {!! $post->body  !!}
+    </div>
+    <a href="/">Go Back</a>
+</article>
+@endsection
+   
+    
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,26 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="/css/app.css">
-    <script src="/js/app.js"></script>
-</head>
-<body>
-    <?php foreach ($posts as $post) :    ?>
+@extends('layout')
+
+@section('content')
+     @foreach ($posts as $post) 
 
     <article>
-        <a href="/posts/<?= $post->slug; ?>">
-            <h1><?= $post->title ?></h1>
+        <a href="/posts/{{ $post->slug }}">
+            <h1>{{ $post->title }}</h1>
         </a>
         <div>
-            <?= $post->body ?>
+            {!! $post->body !!}
         </div>
     </article>
     
-    <?php endforeach; ?>
+    @endforeach
+@endsection
+   
     
-</body>
-</html>
