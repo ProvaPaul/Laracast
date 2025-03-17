1b39fe97a6c886fc106c87d62866fe44f283c309
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2025-03-17 14:06:39 +0600

    convert the html and css to blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,19 @@
-@extends('layout')
-
+@extends('components.layout')
 @section('content')
-     @foreach ($posts as $post) 
+@include('_post-header')
 
-    <article>
-        <a href="/posts/{{ $post->slug }}">
-            <h1>{{ $post->title }}</h1>
-        </a>
-        <p>
-            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-        </p>
-        <div>
-            {!! $post->body !!}
-        </div>
-    </article>
-    
-    @endforeach
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+   <x-post-featured-card />
+
+    <div class="lg:grid lg:grid-cols-2">
+        <x-post-card />
+        <x-post-card />
+    </div>
+
+    <div class="lg:grid lg:grid-cols-3">
+       <x-post-card />
+         <x-post-card />
+            <x-post-card />
+    </div>
+</main>
 @endsection
-   
-    
